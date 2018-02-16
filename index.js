09b00a15616a59f51310ded03a441d9e9c06c19a
Meeta Ahlawat <meetaahlawat@gmail.com>
2018-02-15 22:05:01 -0800

    monthly interest calculation

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ require('mahrio').runServer(process.env, __dirname)
                 var numYearsToPayback = request.payload.numYearsToPayback;
                 var rateOfInterest = request.payload.rateOfInterest;
 
+                // get the principal
+                var principal = parseFloat(loanAmount);
+
+                // Convert interest from a percentage to a decimal, and convert from
+                var interest = parseFloat(rateOfInterest) / 100 / 12;
+
+                // an annual rate to a monthly rate. Convert payment period in years to the number of monthly payments.
+                var payments = parseFloat(numYearsToPayback) * 12;
+
+                // compute the monthly payment figure
+                var x = Math.pow(1 + interest, payments);
+                var monthlyPayment = (principal*x*interest)/(x-1);
+
                 var response = {
-                    monthlyCarPayment: 667.00
+                    monthlyPayment: parseFloat(monthlyPayment.toFixed(2))
                 };
 
                 return reply(response)
